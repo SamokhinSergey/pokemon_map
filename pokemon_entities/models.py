ada01636abe1597c780490153c2e130294634a4a
SERGEY SAMOKHIN <samokhin@live.ru>
2022-05-24 15:03:29 +0400

    add PokemonEntity model.

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -7,3 +7,8 @@ class Pokemon(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class PokemonEntity(models.Model):
+    lat = models.FloatField(max_length=200)
+    lon = models.FloatField(max_length=200)
