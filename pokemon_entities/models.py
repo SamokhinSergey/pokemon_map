1feeac0232b9f6008c96a70a1985695fb2a8b0e6
SERGEY SAMOKHIN <samokhin@live.ru>
2022-05-25 17:09:51 +0400

    Make optional some field in data models

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,19 +3,18 @@ from django.db import models  # noqa F401
 
 class Pokemon(models.Model):
     title = models.CharField(max_length=200,
-                             verbose_name="Имя"
+                             verbose_name="Имя",
                              )
     title_en = models.CharField(max_length=200,
                                 verbose_name="Имя на английском",
-                                blank=True
+                                blank=True,
                                 )
     title_jp = models.CharField(max_length=200,
+                                verbose_name="Имя на японском",
                                 blank=True,
-                                verbose_name="Имя на японском"
                                 )
     image = models.ImageField(upload_to='images',
                               verbose_name="Ссылка на изображение",
-                              blank=True
                               )
     description = models.TextField(blank=True)
     evolution = models.ForeignKey('self',
@@ -23,7 +22,7 @@ class Pokemon(models.Model):
                                   blank=True,
                                   null=True,
                                   related_name='next_evolutions',
-                                  on_delete=models.SET_NULL
+                                  on_delete=models.SET_NULL,
                                   )
 
     def __str__(self):
@@ -33,7 +32,7 @@ class Pokemon(models.Model):
 class PokemonEntity(models.Model):
     Pokemon = models.ForeignKey(Pokemon,
                                 verbose_name="Ссылка на покемона",
-                                on_delete=models.CASCADE
+                                on_delete=models.CASCADE,
                                 )
     lat = models.FloatField(max_length=200,
                             verbose_name="Широта",
@@ -48,20 +47,25 @@ class PokemonEntity(models.Model):
                                           verbose_name="Время исчезновения",
                                           )
     level = models.IntegerField(default=0,
+                                blank=True,
                                 verbose_name="Уровень",
                                 )
     health = models.IntegerField(default=0,
+                                 blank=True,
                                  verbose_name="Здоровье",
                                  )
     strength = models.IntegerField(default=0,
+                                   blank=True,
                                    verbose_name="Сила",
                                    )
     defence = models.IntegerField(default=0,
+                                  blank=True,
                                   verbose_name="Защита",
                                   )
     stamina = models.IntegerField(default=0,
+                                  blank=True,
                                   verbose_name="Выносливость",
                                   )
 
     def __str__(self):
-        return f'{self.Pokemon.title} {self.level} lvl appeared: {self.appeared_at} '
+        return f'{self.Pokemon.title} {self.level} уровень, появится: {self.appeared_at}'
