b4e435e28dd8626a50e4ddbf4f064e0b667b49b8
SERGEY SAMOKHIN <samokhin@live.ru>
2022-05-25 17:02:08 +0400

    add verbose names to all string in models

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -2,28 +2,66 @@ from django.db import models  # noqa F401
 
 
 class Pokemon(models.Model):
-    title = models.CharField(max_length=200)
-    title_en = models.CharField(max_length=200, blank=True)
-    title_jp = models.CharField(max_length=200, blank=True)
-    image = models.ImageField(upload_to='images', blank=True)
+    title = models.CharField(max_length=200,
+                             verbose_name="Имя"
+                             )
+    title_en = models.CharField(max_length=200,
+                                verbose_name="Имя на английском",
+                                blank=True
+                                )
+    title_jp = models.CharField(max_length=200,
+                                blank=True,
+                                verbose_name="Имя на японском"
+                                )
+    image = models.ImageField(upload_to='images',
+                              verbose_name="Ссылка на изображение",
+                              blank=True
+                              )
     description = models.TextField(blank=True)
-    evolution = models.ForeignKey('self', blank=True, null=True, on_delete=models.SET_NULL)
+    evolution = models.ForeignKey('self',
+                                  verbose_name="Из кого эволюционировал",
+                                  blank=True,
+                                  null=True,
+                                  related_name='next_evolutions',
+                                  on_delete=models.SET_NULL
+                                  )
 
     def __str__(self):
         return self.title
 
 
 class PokemonEntity(models.Model):
-    Pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
-    lat = models.FloatField(max_length=200)
-    lon = models.FloatField(max_length=200)
-    appeared_at = models.DateTimeField(default=None)
-    disappeared_at = models.DateTimeField(default=None)
-    level = models.IntegerField(default=0)
-    health = models.IntegerField(default=0)
-    strength = models.IntegerField(default=0)
-    defence = models.IntegerField(default=0)
-    stamina = models.IntegerField(default=0)
+    Pokemon = models.ForeignKey(Pokemon,
+                                verbose_name="Ссылка на покемона",
+                                on_delete=models.CASCADE
+                                )
+    lat = models.FloatField(max_length=200,
+                            verbose_name="Широта",
+                            )
+    lon = models.FloatField(max_length=200,
+                            verbose_name="Долгота",
+                            )
+    appeared_at = models.DateTimeField(default=None,
+                                       verbose_name="Время появления",
+                                       )
+    disappeared_at = models.DateTimeField(default=None,
+                                          verbose_name="Время исчезновения",
+                                          )
+    level = models.IntegerField(default=0,
+                                verbose_name="Уровень",
+                                )
+    health = models.IntegerField(default=0,
+                                 verbose_name="Здоровье",
+                                 )
+    strength = models.IntegerField(default=0,
+                                   verbose_name="Сила",
+                                   )
+    defence = models.IntegerField(default=0,
+                                  verbose_name="Защита",
+                                  )
+    stamina = models.IntegerField(default=0,
+                                  verbose_name="Выносливость",
+                                  )
 
     def __str__(self):
-        return f'{self.Pokemon.title} {self.level} lvl appered: {self.appeared_at} '
+        return f'{self.Pokemon.title} {self.level} lvl appeared: {self.appeared_at} '
--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -1,5 +1,5 @@
 import folium
-import json
+
 
 from django.utils.timezone import localtime
 from django.http import HttpResponseNotFound
@@ -57,6 +57,17 @@ def show_pokemon(request, pokemon_id):
         requested_pokemon = Pokemon.objects.get(id=pokemon_id)
     except Pokemon.DoesNotExist:
         return HttpResponseNotFound('<h1>Такой покемон не найден</h1>')
+
+    related_pokemons = requested_pokemon.next_evolutions.all()
+    next_evolution = {}
+    for related_pokemon in related_pokemons:
+        if related_pokemon:
+            next_evolution = {
+                'title_ru': related_pokemon.title,
+                'pokemon_id': related_pokemon.id,
+                'img_url': request.build_absolute_uri(f'/media/{related_pokemon.image}'),
+            }
+
     previous_evolution = {}
     if requested_pokemon.evolution:
         previous_evolution = {
@@ -71,7 +82,8 @@ def show_pokemon(request, pokemon_id):
         'pokemon_id': requested_pokemon.id,
         'img_url': request.build_absolute_uri(f'/media/{requested_pokemon.image}'),
         'description': requested_pokemon.description,
-        'previous_evolution': previous_evolution
+        'next_evolution': next_evolution,
+        'previous_evolution': previous_evolution,
     }
     pokemons_entity = PokemonEntity.objects.filter(Pokemon__id=pokemon_id)
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
